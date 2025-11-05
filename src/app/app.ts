cf7e56becff2c2a2248924962c190e3b59cd198f
Gustavo de Souza <58256690+GustavoSou2@users.noreply.github.com>
2025-11-05 18:47:15 -0300

    add favicon

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('gustavo-souza-portfolio');
+  protected readonly title = signal('G17 Studio');
 }
