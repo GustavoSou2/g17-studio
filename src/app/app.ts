3ada2f1d3574b56e9b0f665fe526d5863525f2b2
Gustavo de Souza <58256690+GustavoSou2@users.noreply.github.com>
2025-11-07 11:08:53 -0300

    added initial animation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,21 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, type AfterViewInit, type OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastComponent } from './shared/components/toast/toast.component';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, ToastComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit, AfterViewInit {
   protected readonly title = signal('G17 Studio');
+  ngOnInit() {
+    AOS.init();
+  }
+
+  ngAfterViewInit() {
+    AOS.refresh();
+  }
 }
